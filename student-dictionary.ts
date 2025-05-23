5b270777f5cd9e4b26252c26df3d39cebb38efb7
Simi0M0 <somotoso@Outlook.com>
2025-05-23 16:08:49 -0400

    Student Dictionary
    added two new functions to add a student and filter the list

--- student-dictionary.ts
+++ student-dictionary.ts
@@ -10,3 +10,12 @@ let students: Student[] = [
     {id: 2, name: "Isaac", grade: 11, isEnrolled: true},
     {id: 3, name: "Oyinkansola", grade: 9, isEnrolled: false},
 ]
+
+function addAStudent(student: Student): Student[] {
+    students.push({id: 4, name: "Feyisayo", grade: 8, isEnrolled: false});
+    return students;
+}
+
+function filterByEnrolled(student: Student): Student[] {
+    return students.filter(student => student.isEnrolled === true);
+}
