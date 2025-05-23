362e05ebd08e510957a242887121d9cb1ab187b4
Simi0M0 <somotoso@Outlook.com>
2025-05-23 16:18:21 -0400

    Student Dictionary
    added status field and added filterBySenior function

--- student-dictionary.ts
+++ student-dictionary.ts
@@ -3,19 +3,24 @@ type Student = {
     name: string;
     grade: number;
     isEnrolled: boolean;
+    status: "freshman" | "sophomore" | "junior" | "senior";
 };
 
 let students: Student[] = [
-    {id: 1, name: "Simisola", grade: 12, isEnrolled: true},
-    {id: 2, name: "Isaac", grade: 11, isEnrolled: true},
-    {id: 3, name: "Oyinkansola", grade: 9, isEnrolled: false},
+    {id: 1, name: "Simisola", grade: 12, isEnrolled: true, status: "senior"},
+    {id: 2, name: "Isaac", grade: 11, isEnrolled: true, status: "junior"},
+    {id: 3, name: "Oyinkansola", grade: 9, isEnrolled: false, status: "sophomore"},
 ]
 
 function addAStudent(student: Student): Student[] {
-    students.push({id: 4, name: "Feyisayo", grade: 8, isEnrolled: false});
+    students.push({id: 4, name: "Feyisayo", grade: 8, isEnrolled: false, status: "freshman"});
     return students;
 }
 
 function filterByEnrolled(student: Student): Student[] {
     return students.filter(student => student.isEnrolled === true);
+}
+
+function filterBySenior(student: Student): Student[] {
+    return students.filter(student => student.status === "senior");
 }
